bbcc139b129efd69543feb5fd90915c8b7782d2e
arjunsingh14 <arjunsingh140601@gmail.com>
2022-08-10 19:15:06 +0800

    Added orbiting camera and dampening on the cube

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -12,6 +13,14 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+//Cursor
+const cursor = {x: 0, y: 0}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = - (event.clientY / sizes.height - 0.5);
+
+    console.log(cursor.x, cursor.y);
+})
 
 // Sizes
 const sizes = {
@@ -20,9 +29,14 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.height / sizes.width
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -40,11 +54,13 @@ renderer.render(scene, camera)
 const clock = new THREE.Clock()
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-
-    mesh.rotation.y = elapsedTime 
-    mesh.rotation.x = elapsedTime
-    mesh.position.y = Math.sin(elapsedTime)
-    mesh.position.x = Math.cos(elapsedTime)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+    // camera.position.y = cursor.y * 3;
+    //  camera.lookAt(mesh.position);
+    // mesh.rotation.y = elapsedTime 
+    // mesh.rotation.x = elapsedTime
+    controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
