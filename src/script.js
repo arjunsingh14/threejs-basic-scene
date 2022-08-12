8a5a3a75eee2f417fa302774b1ddfbfadbaf5e5d
arjunsingh14 <arjunsingh140601@gmail.com>
2022-08-12 23:03:55 +0800

    Added textures to the cube

--- src/script.js
+++ src/script.js
@@ -1,93 +1,119 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+const imageSource = "/image.png";
 
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-//resize the page
-
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+/**
+ * loading textures
+ */
 
-// Object
+
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => {
+  console.log("loading started");
+};
+loadingManager.onLoad = () => {
+  console.log("loading finished");
+};
+loadingManager.onProgress = () => {
+  console.log("loading progressing");
+};
+loadingManager.onError = () => {
+  console.log("loading error");
+};
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+// ...
+
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+/**
+ * Object
+ */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: heightTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
-//Cursor
-const cursor = {x: 0, y: 0}
-window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = - (event.clientY / sizes.height - 0.5);
 
-    console.log(cursor.x, cursor.y);
-})
 
-// Sizes
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-// Camera
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// const aspectRatio = sizes.height / sizes.width
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
-const controls = new OrbitControls(camera, canvas);
+// Controls
+const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-// controls.autoRotate = true
-// controls.autoRotateSpeed = 10.0
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-  //updates the cameras aspect ratio
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.render(scene, camera);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-
-window.addEventListener("dblclick", () => {
-  if (!document.fullscreenElement){
-      canvas.requestFullscreen()
-  }
-  else {
-    document.exitFullscreen()
-  }
-});
-
-// gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
-// gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
-
-//animations
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
-const tick = () => {
+
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-    // camera.position.y = cursor.y * 3;
-    //  camera.lookAt(mesh.position);
-    // mesh.rotation.y = elapsedTime 
-    // mesh.rotation.x = elapsedTime
+
+    // Update controls
     controls.update()
+
+    // Render
     renderer.render(scene, camera)
+
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
